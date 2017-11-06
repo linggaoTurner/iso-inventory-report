c9b7e63c0325db0762190f63287e9c0964b8a434
Ling Gao <ling.gao@turner.com>
2017-11-06 15:17:40 +0000

    deployment setting

--- inventoryReport/wsgi.py
+++ inventoryReport/wsgi.py
@@ -1,15 +1,16 @@
-"""
-WSGI config for inventoryReport project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
-import os
+import os,sys,site
 
 from django.core.wsgi import get_wsgi_application
+
+site.addsitedir("/home/ubuntu/.virutalenvs/iso-inventory-report/lib/python3.5/site-packages")
+
+sys.path.append("/home/ubuntu/iso-inventory-report")
+sys.path.append("/home/ubuntu/iso-inventory-report/inventoryReport")
+
+activate_this = "/home/ubuntu/.virtualenvs/iso-inventory-report/bin/activate_this.py"
+with open(activate_this) as f:
+    code = compile(f.read(), activate_this, "exec")
+    exec(code, dict(__file__=activate_this))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inventoryReport.settings")
 
